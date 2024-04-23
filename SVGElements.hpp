0476cfb6863d43aa97845db9a10ecbedc0ed98ba
Atum555 <claudiomeireles555@gmail.com>
2024-04-23 13:38:49 +0100

    Create Transform Class

--- SVGElements.hpp
+++ SVGElements.hpp
@@ -1,28 +1,48 @@
-//! @file shape.hpp
+/// @file shape.hpp
 #ifndef __svg_SVGElements_hpp__
 #define __svg_SVGElements_hpp__
 
 #include "Color.hpp"
 #include "PNGImage.hpp"
 #include "Point.hpp"
+#include "string"
 
 namespace svg {
-class SVGElement {
-  public:
-    SVGElement();
-    virtual ~SVGElement();
-    virtual void draw(PNGImage &img) const = 0;
-};
-
-// Declaration of namespace functions
-// readSVG -> implement it in readSVG.cpp
-// convert -> already given (DO NOT CHANGE) in convert.cpp
 
 void readSVG(
     const std::string &svg_file, Point &dimensions,
     std::vector<SVGElement *> &svg_elements
 );
 void convert(const std::string &svg_file, const std::string &png_file);
+
+class Transform {
+    /// @brief Object that represents a transformation.
+    /// @param tx   Translation in X
+    /// @param ty   Translation in Y
+    /// @param r    Rotation
+    /// @param s    Scale
+    /// @param ox   Origin X  
+    /// @param oy   Origin Y
+    Transform(int tx, int ty, int r, int s, int ox, int oy)
+        : transX_(tx), transY_(ty), rotate_(r), scale_(s), origX_(ox),
+          origY_(oy) {}
+
+  private:
+    int transX_, transY_;
+    int rotate_;
+    int scale_;
+    int origX_, origY_;
+};
+
+class SVGElement {
+  public:
+    SVGElement();
+    virtual ~SVGElement();
+    virtual void draw(PNGImage &img) const = 0;
+
+  protected:
+    std::string id;
+};
 
 class Ellipse : public SVGElement {
   public:
